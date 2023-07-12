8df94fa3b7500dba5f54cff78a2306583c214d7d
Kwangsu-CHOI <wisecks@gmail.com>
2023-07-12 13:06:50 +1000

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -64,7 +64,7 @@ function App() {
       // Save user in MongoDB
       if (profileObj) {
         const response = await fetch(
-          "http://localhost:8080/api/v1/users",
+          "https://myboard.onrender.com/api/v1/users",
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -134,7 +134,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://myboard.onrender.com/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
